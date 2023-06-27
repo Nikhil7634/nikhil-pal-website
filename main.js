2d135c3f3738a73257aacbcb1a1f0899119f104c
unknown <paln8n7634@gmail.com>
2023-06-26 22:47:07 -0900

    completed

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ var b = document.getElementById('menu');
 b.onclick = function(){
   a.classList.toggle("slide");
 }
+
+
+b.style.filter = "invert(1)";
+
+var light = document.querySelector(".theme");
+
+light.onclick = function(){
+ document.body.classList.toggle("light-theme");
+ if(b.style.filter == "invert(1)"){
+   b.style.filter = "invert(0)";
+   light.style.filter = "invert(0)";
+   light.style.backgroundImage = "url(moon.png)";
+ }
+ else{
+   b.style.filter = "invert(1)";
+   light.style.filter = "invert(1)";
+   light.style.backgroundImage = "url(sun.png)";
+ }
+   
+}
